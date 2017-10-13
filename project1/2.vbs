2b3decd60a351807d7dca1bcb2e9caa52ea59345
zxy <592657295@qq.com>
2017-10-13 10:37:03 +0800

    1.vbs 2.vbs effective.bat effectiven.bat

--- project1/2.vbs
+++ project1/2.vbs
@@ -1,2 +1,3 @@
 set ws=WScript.CreateObject("WScript.Shell")
 ws.Run "D:\java-neon\workplace\project1\effectiven.bat",0
+�ǷǺڻظ�
--- project1/effective.bat
+++ project1/effective.bat
@@ -1,4 +1,1 @@
 dot C:/Users/Administrator/Desktop/1.dot -Tjpg -o image.jpg
-start D:/java-neon/workplace/project1/image.jpg
-cd C:/Users/Administrator/Desktop
-del 1.dot
